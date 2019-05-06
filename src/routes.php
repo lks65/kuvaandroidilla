66957a9ab24b6bafcfc918694cb76c2bdc04ebf7
Lasse Kelottijärvi <lasse.kelottijarvi@smartpickbox.com>
2019-05-06 12:05:44 +0300

    default route

--- src/routes.php
+++ src/routes.php
@@ -7,26 +7,12 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-
-    $app->get('/page', function (Request $request, Response $response, array $args) use ($container) {
-
-        return $container->get('renderer')->render($response, 'page.phtml', $args);
+    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/' route");
+        // Render index view
+        return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
 
-    $app->get('/download', function() use ($app) {
-
-      $file = "http://www.format-papier-a0-a1-a2-a3-a4-a5.fr/format-a4/a4.jpg";
-
-      $image = dirname(__FILE__).DIRECTORY_SEPARATOR.’my_image.png’;
-      $pdf = new FPDF();
-      $pdf->AddPage();
-      $pdf->Image($file,0,0,210,297);
-      $document = $pdf->Output();
-
-      header("Content-Description: File Transfer");
-      header("Content-Type: application/pdf");
-      header("Content-Disposition: attachment; filename=" . A4);
-
-      readfile ($document);
-    });
+    
 };
